36506c5da1112560f6ee68bd2021e805650057ec
K-RutviK-K <rutvik.khakhrodiya@gmail.com>
2023-06-30 14:51:34 +0530

    remove auto generated folder and files Added token based auth

--- NeuralSpeak.web/NeuralSpeak.Web/Controllers/HomeController.cs
+++ NeuralSpeak.web/NeuralSpeak.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NeuralSpeak.Web.Models;
+using System.Configuration;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace NeuralSpeak.Web.Controllers
 {
@@ -31,8 +33,26 @@ namespace NeuralSpeak.Web.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult TextToSpeech()
+        public async Task<IActionResult> TextToSpeechAsync()
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            var name = config["SubscriptionKey"];
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://eastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken");
+            //request.Headers.Add("Content-type", "application/x-www-form-urlencoded");
+            //request.Headers.Add("Content-length", "0");            //request.Headers.Add("Content-type", "application/x-www-form-urlencoded");
+            //request.Headers.Add("Content-length", "0");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", "a6cf9b866ee640cca1e58c23c138a702");
+            var content = new StringContent(string.Empty);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            request.Content = content;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            //Console.WriteLine(await response.Content.ReadAsStringAsync());
+            ViewBag.token = await response.Content.ReadAsStringAsync();
             return View();
         }
 
